deaa201fa3f46c8de87283aa22639a01c3de9b39
ramesh <ramesh.selvam27@gmail.com>
2020-12-09 20:29:35 +0530

    commit form IST time zone

--- alerts.py
+++ alerts.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import pytest
 
 driver = webdriver.Chrome(executable_path= "C:\Drivers\chromedriver")
 driver.get("https://rahulshettyacademy.com/AutomationPractice/")
